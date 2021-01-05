f5e94bce0f438165d7b705db0ba15ded30bcffe9
Raymond Seger <raymondSeger@live.com>
2021-01-05 17:17:13 +0700

    YoYo Animation

---
+++ app/src/main/java/com/example/myapplication/YoyoActivity.java
@@ -0,0 +1,36 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+
+public class YoyoActivity extends AppCompatActivity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yoyo);
+
+        mTextView               = findViewById(R.id.text_view);
+        Button buttonAnimate    = findViewById(R.id.button_animate);
+
+        buttonAnimate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                YoYo.with(Techniques.Tada)
+                        .duration(700)
+                        .repeat(1)
+                        .playOn(mTextView);
+            }
+        });
+
+    }
+}
