6f336a01c111cd360dfef242445ab1bcce966b05
Raymond Seger <raymondSeger@live.com>
2021-01-07 14:02:25 +0700

    Spinner

---
+++ app/src/main/java/com/example/myapplication/SpinnerActivity.java
@@ -0,0 +1,38 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class SpinnerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spinner);
+
+        Spinner spinner                     = findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence> adapter  = new ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_item, new String[]{"Food", "Drink", "Milk", "Discount", "Food 2", "Drink 2", "Milk 2", "Discount 2"});
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String text = adapterView.getItemAtPosition(i).toString();
+                Toast.makeText(adapterView.getContext(), text, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
+}
