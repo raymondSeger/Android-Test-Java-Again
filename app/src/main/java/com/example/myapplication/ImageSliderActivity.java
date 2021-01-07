0832b1e7dcf8cdec400405516865dd7ff35a5b66
Raymond Seger <raymondSeger@live.com>
2021-01-07 12:58:22 +0700

    ImageSliderActivity

---
+++ app/src/main/java/com/example/myapplication/ImageSliderActivity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.example.myapplication.adapters.ImageAdapter;
+
+public class ImageSliderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_slider);
+
+        ViewPager viewPager     = findViewById(R.id.viewPager);
+        ImageAdapter adapter    = new ImageAdapter(this);
+        viewPager.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/adapters/ImageAdapter.java
@@ -0,0 +1,42 @@
+package com.example.myapplication.adapters;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.example.myapplication.R;
+
+public class ImageAdapter extends PagerAdapter {
+    private Context mContext;
+    private int[] mImageIds = new int[]{R.drawable.flag_afghanistan, R.drawable.flag_albania, R.drawable.flag_algeria};
+
+    public ImageAdapter(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public int getCount() {
+        return mImageIds.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        ImageView imageView = new ImageView(mContext);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setImageResource(mImageIds[position]);
+        container.addView(imageView, 0);
+        return imageView;
+    }
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((ImageView) object);
+    }
+}
