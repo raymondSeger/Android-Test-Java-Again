45acdcbd4d85fb9fc01979392153b6b1ac0cb806
Raymond Seger <raymondSeger@live.com>
2021-01-10 16:14:09 +0700

    ChooseColor dialog

---
+++ app/src/main/java/com/example/myapplication/ChooseColorActivity.java
@@ -0,0 +1,50 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import yuku.ambilwarna.AmbilWarnaDialog;
+
+public class ChooseColorActivity extends AppCompatActivity {
+
+    ConstraintLayout mLayout;
+    int mDefaultColor;
+    Button mButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_color);
+
+        mLayout         = (ConstraintLayout) findViewById(R.id.layout);
+        mDefaultColor   = ContextCompat.getColor(ChooseColorActivity.this, R.color.colorPrimary);
+        mButton         = (Button) findViewById(R.id.button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openColorPicker();
+            }
+        });
+
+    }
+
+    public void openColorPicker() {
+        AmbilWarnaDialog colorPicker = new AmbilWarnaDialog(this, mDefaultColor, new AmbilWarnaDialog.OnAmbilWarnaListener() {
+            @Override
+            public void onCancel(AmbilWarnaDialog dialog) {
+            }
+            @Override
+            public void onOk(AmbilWarnaDialog dialog, int color) {
+                mDefaultColor = color;
+                mLayout.setBackgroundColor(mDefaultColor);
+            }
+        });
+        colorPicker.show();
+    }
+
+}
