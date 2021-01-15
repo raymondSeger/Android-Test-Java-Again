971d8bd404a3b4231ae3f87fdc4c6cf950168b46
Raymond Seger <raymondSeger@live.com>
2021-01-15 17:32:40 +0700

    extra data

--- app/src/main/java/com/example/myapplication/DeepLinkActivity.java
+++ app/src/main/java/com/example/myapplication/DeepLinkActivity.java
@@ -21,6 +21,7 @@ public class DeepLinkActivity extends AppCompatActivity {
         Uri data        = intent.getData(); // https://www.example.com/hello/world?a=12
 
         // Try with adb shell am start -W -a android.intent.action.VIEW -d "https://www.example.com/hello/world?a=12" com.example.myapplication
+        // in real world, add this to site: https://developer.android.com/training/app-links/verify-site-associations
         if(data != null) {
             textView5.setText( data.toString() );
         }
