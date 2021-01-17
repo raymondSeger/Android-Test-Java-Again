fe90d6afe6a08605444e3f12dc1addb0cad109d4
Raymond Seger <raymondSeger@live.com>
2021-01-17 16:26:09 +0700

    find bluetooth device

--- app/src/main/java/com/example/myapplication/BluetoothOnOrOffActivity.java
+++ app/src/main/java/com/example/myapplication/BluetoothOnOrOffActivity.java
@@ -3,11 +3,14 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import java.util.Set;
 
 public class BluetoothOnOrOffActivity extends AppCompatActivity {
 
@@ -16,8 +19,9 @@ public class BluetoothOnOrOffActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth_on_or_off);
 
-        Button btntOn                   = (Button)findViewById(R.id.btnOn);
-        Button btntOff                  = (Button)findViewById(R.id.btnOFF);
+        Button btntOn                   = (Button) findViewById(R.id.btnOn);
+        Button btntOff                  = (Button) findViewById(R.id.btnOFF);
+        Button findDevice               = (Button) findViewById(R.id.findDevice);
         final BluetoothAdapter bAdapter = BluetoothAdapter.getDefaultAdapter();
 
         btntOn.setOnClickListener(new View.OnClickListener() {
@@ -39,6 +43,29 @@ public class BluetoothOnOrOffActivity extends AppCompatActivity {
             public void onClick(View v) {
                 bAdapter.disable();
                 Toast.makeText(getApplicationContext(),"Bluetooth Turned OFF", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        findDevice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if( bAdapter.isEnabled() ) {
+
+                    Set<BluetoothDevice> pairedDevices = bAdapter.getBondedDevices();
+                    if (pairedDevices.size() > 0) {
+                        // There are paired devices. Get the name and address of each paired device.
+                        for (BluetoothDevice device : pairedDevices) {
+                            String deviceName               = device.getName();
+                            String deviceHardwareAddress    = device.getAddress(); // MAC address
+
+                            // debug here
+                            int a = 0;
+                        }
+                    }
+
+                }
+
             }
         });
 
