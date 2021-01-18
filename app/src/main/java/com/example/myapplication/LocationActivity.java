153bd2185c721005d5ba79aa8dff90db47f2d19d
Raymond Seger <raymondSeger@live.com>
2021-01-18 14:08:04 +0700

    interval + geolocation

--- app/src/main/java/com/example/myapplication/LocationActivity.java
+++ app/src/main/java/com/example/myapplication/LocationActivity.java
@@ -1,17 +1,29 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
@@ -21,6 +33,8 @@ import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 public class LocationActivity extends AppCompatActivity {
 
     private FusedLocationProviderClient fusedLocationClient;
+    private Location locationRequest;
+    private LocationRequest mLocationRequest;
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
@@ -28,6 +42,22 @@ public class LocationActivity extends AppCompatActivity {
             case 1:
                 if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     getCurrentLocation();
+                } else {
+                    // Permission Denied
+                    Toast.makeText(getBaseContext(), "not allowed, app should exit", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case 2:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getCurrentLocationGooglePlay();
+                } else {
+                    // Permission Denied
+                    Toast.makeText(getBaseContext(), "not allowed, app should exit", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case 3:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startLocationUpdates();
                 } else {
                     // Permission Denied
                     Toast.makeText(getBaseContext(), "not allowed, app should exit", Toast.LENGTH_SHORT).show();
@@ -46,8 +76,56 @@ public class LocationActivity extends AppCompatActivity {
         // get the current location
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
+        // using fused
         getCurrentLocation();
 
+        // using google play
+        // getCurrentLocationGooglePlay();
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        boolean requestingLocationUpdates = true;
+
+        if (requestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    private void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(LocationActivity.this, new String[] { ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION }, 3);
+            return;
+        }
+
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(3000);
+        mLocationRequest.setFastestInterval(2000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        // will keep running this function
+        fusedLocationClient.requestLocationUpdates(mLocationRequest,
+                new LocationCallback(){
+                    @Override
+                    public void onLocationResult(LocationResult locationResult) {
+                        if (locationResult == null) {
+                            return;
+                        }
+                        for (Location location : locationResult.getLocations()) {
+                            float accuracy      = location.getAccuracy();
+                            double altitude     = location.getAltitude();
+                            double latitude     = location.getLatitude();
+                            double longitude    = location.getLongitude();
+                            String provider     = location.getProvider();
+                            long time           = location.getTime();
+                        }
+                    }
+                },
+                Looper.getMainLooper());
     }
 
     private void getCurrentLocation() {
@@ -61,6 +139,7 @@ public class LocationActivity extends AppCompatActivity {
             .addOnSuccessListener(this, new OnSuccessListener<Location>() {
                 @Override
                 public void onSuccess(Location location) {
+                    locationRequest = location;
                     // Got last known location. In some rare situations this can be null.
                     if (location != null) {
                         float accuracy      = location.getAccuracy();
@@ -73,6 +152,43 @@ public class LocationActivity extends AppCompatActivity {
                 }
             });
 
+    }
+
+    private void getCurrentLocationGooglePlay() {
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+
+        SettingsClient client = LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+        task.addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                boolean gps_present = locationSettingsResponse.getLocationSettingsStates().isGpsPresent();
+            }
+        });
+
+        task.addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if (e instanceof ResolvableApiException) {
+                    // Location settings are not satisfied, but this can be fixed
+                    // by showing the user a dialog.
+                    try {
+                        // Show the dialog by calling startResolutionForResult(),
+                        // and check the result in onActivityResult().
+                        ResolvableApiException resolvable = (ResolvableApiException) e;
+                        resolvable.startResolutionForResult(LocationActivity.this, 2);
+                    } catch (IntentSender.SendIntentException sendEx) {
+                        // Ignore the error.
+                    }
+                }
+            }
+        });
     }
 
 }
