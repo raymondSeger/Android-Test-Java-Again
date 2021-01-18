f6a8750977df21b9c294222accc3e5728092ec0f
Raymond Seger <raymondSeger@live.com>
2021-01-18 15:20:17 +0700

    motion layout

---
+++ app/src/main/java/com/example/myapplication/MotionTagActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MotionTagActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // setContentView(R.layout.activity_motion_tag);
+        // setContentView(R.layout.activity_motion_03_custom_attribute);
+        setContentView(R.layout.activity_motion_05_imagefilter);
+    }
+}
