3d0b79ee869fc0244ad789f78f88df2e995a05c5
Raymond Seger <raymondSeger@live.com>
2021-01-19 16:21:36 +0700

    RemoteConfig Firebase

---
+++ app/src/main/java/com/example/myapplication/RemoteConfigActivity.java
@@ -0,0 +1,35 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+
+public class RemoteConfigActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remote_config);
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+
+        FirebaseRemoteConfig mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+        FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
+                .setMinimumFetchIntervalInSeconds(3600)
+                .build();
+        mFirebaseRemoteConfig.setConfigSettingsAsync(configSettings);
+
+        String app_version = mFirebaseRemoteConfig.getString("app_version_from_remoteconfig");
+        textView.setText(app_version);
+
+    }
+}
