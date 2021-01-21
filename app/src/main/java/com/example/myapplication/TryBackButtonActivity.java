96158287461f20455e3a95d5f596a77c2bb4063c
Raymond Seger <raymondSeger@live.com>
2021-01-21 14:39:31 +0700

    test stack ( back ) on activity

---
+++ app/src/main/java/com/example/myapplication/TryBackButtonActivity.java
@@ -0,0 +1,60 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class TryBackButtonActivity extends AppCompatActivity {
+
+    private Button goToActivity1;
+    private Button goToActivity2;
+    private Button goToActivity3;
+    private Button goToActivity4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_try_back_button);
+
+        goToActivity1 = (Button) findViewById(R.id.goToActivity1);
+        goToActivity2 = (Button) findViewById(R.id.goToActivity2);
+        goToActivity3 = (Button) findViewById(R.id.goToActivity3);
+        goToActivity4 = (Button) findViewById(R.id.goToActivity4);
+
+        goToActivity1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TryBackButtonActivity.this, TryBackButtonActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        goToActivity2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TryBackButtonActivity.this, TryBackButtonActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+        goToActivity3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TryBackButtonActivity.this, TryBackButtonActivity3.class);
+                startActivity(intent);
+            }
+        });
+
+        goToActivity4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TryBackButtonActivity.this, TryBackButtonActivity4.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
