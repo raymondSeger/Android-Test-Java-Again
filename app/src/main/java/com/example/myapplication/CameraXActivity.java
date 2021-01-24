24007109b5d75a75d810cf01a9b9a4f96ec5c2be
Raymond Seger <raymondSeger@live.com>
2021-01-24 15:39:12 +0700

    CameraX image analyzer

--- app/src/main/java/com/example/myapplication/CameraXActivity.java
+++ app/src/main/java/com/example/myapplication/CameraXActivity.java
@@ -1,8 +1,11 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
@@ -10,17 +13,37 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LifecycleOwner;
 
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.media.Image;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.common.util.concurrent.ListenableFuture;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
 
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static android.Manifest.permission.CAMERA;
 
 public class CameraXActivity extends AppCompatActivity {
+
+    public Camera camera;
+    private ImageAnalysis imageAnalyzer;
+    public Toast toast_object = null;
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
@@ -64,7 +87,59 @@ public class CameraXActivity extends AppCompatActivity {
                         .build();
 
                 preview.setSurfaceProvider(viewFinder.getSurfaceProvider());
-                Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, preview);
+
+                ImageAnalysis imageAnalyzer = new ImageAnalysis.Builder().build();
+                Executor the_executor       = new Executor() {
+                    @Override
+                    public void execute(Runnable command) {
+                        command.run();
+                    }
+                };
+
+                imageAnalyzer.setAnalyzer(the_executor, new ImageAnalysis.Analyzer() {
+                    @Override
+                    public void analyze(@NonNull ImageProxy imageProxy) {
+                        @SuppressLint("UnsafeExperimentalUsageError") Image mediaImage = imageProxy.getImage();
+                        if (mediaImage != null) {
+                            InputImage image = InputImage.fromMediaImage(mediaImage, imageProxy.getImageInfo().getRotationDegrees());
+                            // Pass image to an ML Kit Vision API
+
+                            TextRecognizer recognizer   = TextRecognition.getClient();
+                            Task<Text> result           = recognizer.process(image).addOnSuccessListener(new OnSuccessListener<Text>() {
+                                @Override
+                                public void onSuccess(Text visionText) {
+                                    // Task completed successfully
+                                    String resultText = visionText.getText();
+                                    if(!resultText.equals("") && resultText != null ) {
+                                        Log.d("resultText", resultText);
+
+                                        if(toast_object != null) {
+                                            toast_object.cancel();
+                                        }
+
+                                        toast_object = Toast.makeText(CameraXActivity.this, resultText, Toast.LENGTH_SHORT);
+                                        toast_object.show();
+                                    }
+                                    imageProxy.close();
+
+                                }
+                            }) .addOnFailureListener(
+                                new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        // Task failed with an exception
+                                        Toast.makeText(CameraXActivity.this, "failed", Toast.LENGTH_SHORT).show();
+                                        imageProxy.close();
+                                    }
+                            });
+
+                        }
+                    }
+                });
+
+                camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, preview, imageAnalyzer);
+
+
             } catch (ExecutionException | InterruptedException e) {
                 // No errors need to be handled for this Future.
                 // This should never be reached.
