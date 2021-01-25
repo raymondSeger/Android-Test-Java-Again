ff3bc4eb4028d0e28d4d60c80b41aaaff09bdb7f
Raymond Seger <raymondSeger@live.com>
2021-01-25 15:21:03 +0700

    READ_SMS & SEND_SMS

---
+++ app/src/main/java/com/example/myapplication/ReadWriteSMSActivity.java
@@ -0,0 +1,125 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import static android.Manifest.permission.READ_SMS;
+import static android.Manifest.permission.SEND_SMS;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+public class ReadWriteSMSActivity extends AppCompatActivity {
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    readSMS();
+                } else {
+                    // Permission Denied
+                    Toast.makeText(getBaseContext(), "not allowed, app should exit", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case 2:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    writeSMS();
+                } else {
+                    // Permission Denied
+                    Toast.makeText(getBaseContext(), "not allowed, app should exit", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read_write_receive_sms);
+
+        Button read_sms_btn     = (Button) findViewById(R.id.read_sms_btn);
+        Button write_sms_btn    = (Button) findViewById(R.id.write_sms_btn);
+
+        read_sms_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (ActivityCompat.checkSelfPermission(ReadWriteSMSActivity.this, READ_SMS) != PackageManager.PERMISSION_GRANTED ) {
+                    ActivityCompat.requestPermissions(ReadWriteSMSActivity.this, new String[] { READ_SMS }, 1);
+                    return;
+                }
+
+                readSMS();
+            }
+        });
+
+        write_sms_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ActivityCompat.checkSelfPermission(ReadWriteSMSActivity.this, SEND_SMS) != PackageManager.PERMISSION_GRANTED ) {
+                    ActivityCompat.requestPermissions(ReadWriteSMSActivity.this, new String[] { SEND_SMS }, 2);
+                    return;
+                }
+
+                writeSMS();
+            }
+        });
+
+    }
+
+    private void writeSMS() {
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage("+6281389688115", null, "Test", null, null);
+            Toast.makeText(getApplicationContext(), "SMS Sent!", Toast.LENGTH_LONG).show();
+        }
+        catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "SMS faild, please try again.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void readSMS() {
+        // Create Inbox box URI
+        Uri inboxURI = Uri.parse("content://sms/inbox");
+
+        // List required columns
+        String[] reqCols = new String[] { "_id", "address", "body", "read", "date", "type"};
+
+        // Get Content Resolver object, which will deal with Content Provider
+        ContentResolver cr = getContentResolver();
+
+        // Fetch Inbox SMS Message from Built-in Content Provider
+        Cursor c        = cr.query(inboxURI, reqCols, null, null, null);
+        int totalSMS    = c.getCount();
+        if (c.moveToFirst()) {
+            for (int i = 0; i < totalSMS; i++) {
+                String id       = c.getString(c.getColumnIndexOrThrow("_id"));
+                String address  = c.getString(c.getColumnIndexOrThrow("address"));
+                String body     = c.getString(c.getColumnIndexOrThrow("body"));
+                String read     = c.getString(c.getColumnIndex("read"));
+                String date     = c.getString(c.getColumnIndex("date"));
+                if (c.getString(c.getColumnIndex("type")).contains("1")) {
+                    String folder_name = "inbox";
+                } else {
+                    String folder_name = "sent";
+                }
+                c.moveToNext();
+            }
+        }
+        else {
+            Toast.makeText(this, "You have no SMS", Toast.LENGTH_SHORT).show();
+        }
+        c.close();
+    }
+}
