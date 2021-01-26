c9c68e13f5e4a3a2a5a879626b75216100549bab
Raymond Seger <raymondSeger@live.com>
2021-01-26 13:29:34 +0700

    set wallpaper

---
+++ app/src/main/java/com/example/myapplication/WallpaperActivity.java
@@ -0,0 +1,31 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.WallpaperManager;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+public class WallpaperActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wallpaper);
+
+        WallpaperManager wallpaper_manager = WallpaperManager.getInstance(getApplicationContext());
+
+        try{
+            wallpaper_manager.setBitmap(
+                    BitmapFactory.decodeResource(getBaseContext().getResources(),
+                            R.drawable.flag_afghanistan) );
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        Toast.makeText(getApplicationContext(),"Wallpaper changed",Toast.LENGTH_SHORT).show();
+    }
+}
