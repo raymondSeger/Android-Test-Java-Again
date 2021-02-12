743055ee00d63dc7cf53669aa26a6ae0e8ab15c1
Raymond Seger <raymondSeger@live.com>
2021-02-12 13:54:28 +0700

    Cell Data

---
+++ app/src/main/java/com/example/myapplication/TelephonyActivity.java
@@ -0,0 +1,60 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.telephony.CellIdentity;
+import android.telephony.CellInfo;
+import android.telephony.CellInfoGsm;
+import android.telephony.CellSignalStrength;
+import android.telephony.CellSignalStrengthGsm;
+import android.telephony.PhoneNumberFormattingTextWatcher;
+import android.telephony.TelephonyManager;
+import android.telephony.UiccCardInfo;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+public class TelephonyActivity extends AppCompatActivity {
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                getSignalData();
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_telephony);
+        EditText editTextPhone = (EditText) findViewById(R.id.editTextPhone);
+        getSignalData();
+    }
+
+    private void getSignalData() {
+        if (ActivityCompat.checkSelfPermission(getBaseContext(), ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
+            ActivityCompat.requestPermissions(TelephonyActivity.this, new String[] { ACCESS_FINE_LOCATION }, 1);
+            return;
+        }
+
+        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        CellInfo cellInfo = (CellInfo) telephonyManager.getAllCellInfo().get(0);
+        cellInfo.getCellIdentity();
+        cellInfo.getCellSignalStrength().getAsuLevel();
+
+        UiccCardInfo a = UiccCardInfo.
+
+    }
+}
